74e2d1600b2d2d278d64ba0578b055dc870f69a9
hardeepkaur <hardeepwalia019@gmail.com>
2019-06-19 13:15:50 -0400

    adding the lemmings

--- Test34-S19/GameScene.swift
+++ Test34-S19/GameScene.swift
@@ -13,14 +13,53 @@ class GameScene: SKScene {
     
     var nextLevelButton:SKLabelNode!
     
+    let dino = SKSpriteNode(imageNamed: "dino")
+    var timeOfLastUpdate:TimeInterval = 0
+    var dt: TimeInterval = 0
+    var wall:SKSpriteNode?
     
     override func didMove(to view: SKView) {
         print("This is level 1")
         self.nextLevelButton = self.childNode(withName: "nextLevelButton") as! SKLabelNode
+        self.wall?.physicsBody?.collisionBitMask = 0
     }
+    func spawndino() {
+        let dino = SKSpriteNode(imageNamed:"frame-1")
+        
+        // put sand at a random (x,y) position
+        let x = self.size.width/2
+        let y = self.size.height - 100
+        dino.position.x = x
+        dino.position.y = y
+        
+        // add physics
+        dino.physicsBody = SKPhysicsBody(circleOfRadius: dino.size.width / 2)
+        self.dino.physicsBody?.affectedByGravity = false
+        var dinoTextures:[SKTexture] = []
+        for i in 1...4 {
+            let fileName = "frame-\(i)"
+            print("Adding: \(fileName) to array")
+            dinoTextures.append(SKTexture(imageNamed: fileName))
+        }
+        
+        let walkingAnimation = SKAction.animate(
+            with: dinoTextures,
+            timePerFrame: 0.1)
+        
+        // 3. Repeat the animation forever
+        addChild(dino)
+        self.dino.run(SKAction.repeatForever(walkingAnimation))
+        
+    }
+    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        self.dt = currentTime - timeOfLastUpdate
+        if (self.dt >= 2.5) {
+            timeOfLastUpdate = currentTime
+            self.spawndino()
+        }
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
@@ -49,3 +88,4 @@ class GameScene: SKScene {
         
     }
 }
+
